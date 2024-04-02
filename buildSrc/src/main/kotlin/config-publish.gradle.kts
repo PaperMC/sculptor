67bb9bcabb095702060112d77985579c297c27ab
MiniDigger | Martin <admin@benndorf.dev>
2024-04-02 21:25:39 +0200

    update diffpatch, publish to our repo

--- buildSrc/src/main/kotlin/config-publish.gradle.kts
+++ buildSrc/src/main/kotlin/config-publish.gradle.kts
@@ -54,7 +54,12 @@ val shadowJar by tasks.existing(ShadowJar::class) {
 
 publishing {
     repositories {
-        maven("https://repo.papermc.io/repository/maven-snapshots/") {
+        val url = if (project.version.toString().endsWith("-SNAPSHOT")) {
+            "https://repo.papermc.io/repository/maven-snapshots/"
+        } else {
+            "https://repo.papermc.io/repository/maven-releases/"
+        }
+        maven(url) {
             credentials(PasswordCredentials::class)
             name = "paper"
         }
