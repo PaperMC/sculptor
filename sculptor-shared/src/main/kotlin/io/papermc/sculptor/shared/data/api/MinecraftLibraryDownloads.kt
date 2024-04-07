822213cea59fc888f3a26fb4778c819be8a846c7
Isaac - The456 <the456@the456gamer.dev>
2024-04-07 18:02:18 +0100

    remove unused mojang launchermeta json field
    previously this was used for getting the library jar, instead of downloading it from mojangs maven

--- sculptor-shared/src/main/kotlin/io/papermc/sculptor/shared/data/api/MinecraftLibraryDownloads.kt
+++
@@ -1,8 +0,0 @@
-package io.papermc.sculptor.shared.data.api
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class MinecraftLibraryDownloads(
-    val artifact: MinecraftDownload,
-)
